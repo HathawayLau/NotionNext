dfb35483b95845add603d847c1442b37afc18eee
Hathaway Lau <160312998+HathawayLau@users.noreply.github.com>
2024-04-19 15:15:33 +0800

    Update config.js

--- themes/example/config.js
+++ themes/example/config.js
@@ -7,7 +7,7 @@ const CONFIG = {
   EXAMPLE_MENU_TAG: true, // 显示标签
   EXAMPLE_MENU_ARCHIVE: true, // 显示归档
   EXAMPLE_MENU_SEARCH: true, // 显示搜索
-
+  THEME_SWITCH: process.env.NEXT_PUBLIC_THEME_SWITCH || true, // 是否显示切换主题按钮
   EXAMPLE_POST_LIST_COVER: true // 列表显示文章封面
 
 }
